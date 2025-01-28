f43411b103cf26ee7b5895986d974e65c56a4359
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-27 21:02:21 -0700

    Initial Commit

---
+++ Jokes4Me/Jokes4MeApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// Jokes4Me
+// Jokes4MeApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct Jokes4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
